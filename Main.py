2dba13bdf85b38962e2d66766d2ecff22aeabc87
reeba24 <100011665+reeba24@users.noreply.github.com>
2022-10-09 10:53:43 +0530

    4th

--- Main.py
+++ Main.py
@@ -4,11 +4,14 @@ def merge_sort(data) -> None:
   # Write code here
   if len(data) > 1:
     mid = len(data)//2
-    left_array = data[:mid]
-    right_array = data[mid:]
+    left= data[:mid]
+    right= data[mid:]
+    
     merge_sort(left)
     merge_sort(right)
+    
     i,j,k=0
+    
     while i < len(left) and j < len(right):
       if left[i] <= right[j]:
         data[k] = left[i]
@@ -17,8 +20,9 @@ def merge_sort(data) -> None:
         data[k] = right[j]
         j += 1
       k += 1
-     while i < len(left_array):
-      data[k] = left_array[i]
+
+    while i < len(left):
+      data[k] = left[i]
       i += 1
       k += 1
 
